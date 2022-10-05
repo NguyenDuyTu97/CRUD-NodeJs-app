8428139b496072f959e2b7358411d87d870febd8
NguyenDuyTu97 <45293691+NguyenDuyTu97@users.noreply.github.com>
2022-10-05 16:02:23 +0700

    init commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//config url view engine
+configViewEngine(app);
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
